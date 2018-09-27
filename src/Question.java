55d0e8dc90efc6f0006468ec75122aabff6aa56d
Taylor <twslusher@bsu.edu>
2018-09-26 21:00:37 -0400

    the Question constructor takes a question as a string, the correct answer as a string, and four seperate answers, each as strings

--- src/Question.java
+++ src/Question.java
@@ -1,7 +1,26 @@
 public class Question {
 
-    public static void question(String question, String answer){
+    public static String theQuestion;
+    public static String correctA;
+    public static String A1;
+    public static String A2;
+    public static String A3;
+    public static String A4;
 
+
+//    The question class takes in strings that are the question,
+//    followed by a string that is the correct answer, and following
+//    that are three incorrect answers each in there own string
+
+
+    public Question(String question, String cAnswer, String Answer1, String Answer2, String Answer3, String Answer4){
+
+        theQuestion = question;
+        correctA = cAnswer;
+        A1 = Answer1;
+        A2 = Answer2;
+        A3 = Answer3;
+        A4 = Answer4;
 
 
     }
--- src/QuestionTest.java
+++ src/QuestionTest.java
@@ -1,8 +1,34 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class QuestionTest {
 
     @org.junit.jupiter.api.Test
     void question() {
+
+
+
     }
+
+    @Test
+    public void testQuestion(){
+        String question = "Is this a test?";
+        String correctAnswer = "yes";
+        String answer1 = "yes";
+        String answer2 = "no";
+        String answer3 = "maybe";
+        String answer4 = "possibly";
+
+        Question Q1 = new Question(question,correctAnswer,answer1,answer2,answer3,answer4);
+
+        assertEquals(question, Q1.theQuestion);
+        assertEquals(answer1, Q1.A1);
+        assertEquals(answer2, Q1.A2);
+        assertEquals(answer3, Q1.A3);
+        assertEquals(answer4, Q1.A4);
+        assertEquals(correctAnswer, Q1.correctA);
+
+    }
+
 }
