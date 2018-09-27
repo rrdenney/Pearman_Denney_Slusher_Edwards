be7713e43d9e678edf8c37ee582edf950d3e51b6
Taylor <twslusher@bsu.edu>
2018-09-26 20:11:27 -0400

    Created the Question constructor class Created the QuestionTest class

---
+++ src/Question.java
@@ -0,0 +1,10 @@
+public class Question {
+
+    public static void question(String question, String answer){
+
+
+
+    }
+
+
+}
---
+++ src/QuestionTest.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionTest {
+
+    @org.junit.jupiter.api.Test
+    void question() {
+    }
+}
